144d31a74219084f385f42de2305c42ac651cdec
schloss-simms <75788577+schloss-simms@users.noreply.github.com>
2021-01-26 11:52:55 +0100

    resubmit lab 9

--- SQL Lab 9, 3.03 - Schloss.sql
+++ SQL Lab 9, 3.03 - Schloss.sql
@@ -21,31 +21,20 @@ inner join actor a2
 on a2.actor_id = fa2.actor_id;
     
 -- Q2 Get all pairs of customers that have rented the same film more than 3 times.
--- Q2 basic code
-select r1.customer_id, r2.customer_id, count(distinct r1.inventory_id) as "overlap_film_counts" 
-from sakila.rental as r1 
-	join sakila.rental as r2 
-	on r1.inventory_id = r2.inventory_id and r1.customer_id < r2.customer_id
-	join sakila.customer_list as c
-	on r1.customer_id = c.id
-group by r1.customer_id, r2.customer_id
-having count(distinct r1.inventory_id) > 3
-order by count(distinct r1.inventory_id) desc;
-
--- Q2 better code with customer names
-select rental.inventory_id, concat(c1.first_name," ", c1.last_name) as c1n, concat(c2.first_name," ", c2.last_name) as c2n, count(distinct r1.inventory_id) as "overlap_film_counts" 
-from rental
-    join sakila.rental as r1
-    on rental.inventory_id = r1.inventory_id
-    join sakila.customer as c1
+-- better code
+select concat(c1.first_name," ", c1.last_name) as Customer1, concat(c2.first_name," ", c2.last_name) as Customer2, count(r.inventory_id) as Overlapping_Film_Count
+from rental as r
+    inner join sakila.rental as r1
+    on r.inventory_id = r1.inventory_id
+    inner join sakila.customer as c1
     on r1.customer_id = c1.customer_id
 	join sakila.rental as r2 
 	on r1.inventory_id = r2.inventory_id and r1.customer_id < r2.customer_id
-    join sakila.customer as c2
+    inner join sakila.customer as c2
     on r2.customer_id=c2.customer_id
 group by r1.customer_id, r2.customer_id
-having count(distinct r1.inventory_id) > 3
-order by count(distinct r1.inventory_id) desc;
+having count(*) > 3
+order by count(*) desc;
 
 -- Q3 Get all possible pairs of actors and films.
 select concat(a.first_name,' ', a.last_name) as actor_name, f.title
